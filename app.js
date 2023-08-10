07673a5611624aa9795876ae48910a4f4985438a
navtej bhatti <navbhtt0@gmail.com>
2023-08-10 17:52:44 +0530

    cors added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const { getMovieReviews, getLatestMovieReviews } = require('./nytapi'); // Import the nytApi module
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("css"));
 
 app.get("/", function (req, res) {
